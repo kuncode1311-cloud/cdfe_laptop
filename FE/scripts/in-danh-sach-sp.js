0bd8fe01cd89b13ce67d4d5660c7c2060f4fc031
trikun <kun.code.1311@gmail.com>
2026-09-22 15:08:20 +0700

    cleanup: xoa cac script cu doc db.json trong FE/scripts

--- FE/scripts/in-danh-sach-sp.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, '../db.json'), 'utf8'));
-db.san_pham.forEach((s, idx) => {
-  const dm = Array.isArray(s.danh_muc) ? s.danh_muc.join(', ') : s.danh_muc;
-  console.log((idx + 1) + '. [' + s.id + '] (' + dm + ') - ' + s.ten_san_pham);
-});
